e177f3da55366e3f0a288a1e30004502d5ba83b2
xandone <765478955@qq.com>
2018-04-19 22:39:44 +0800

    cal

--- cal/js/xmain.js
+++ cal/js/xmain.js
@@ -1,9 +1,56 @@
-	
+	'use strict'
 
-var Main={
-	temp: 0, 
-	description:document.getElementById("sum"),
-	inClick:function (number) {
-		this.description.value=number;
+	var Main = {
+		temp: 0,
+		numA: 0,
+		numB: 0,
+		summer: '',
+
+		result: document.getElementById('sum'),
+
+		inClick: function(number) {
+
+			switch (number) {
+				case '1':
+				case '2':
+				case '3':
+				case '4':
+				case '5':
+				case '6':
+				case '7':
+				case '8':
+				case '9':
+					this.summer += number;
+					break;
+				case '+':
+				case '-':
+				case 'x':
+				case '/':
+					if (this.summer.indexOf('+') != -1 ||
+						this.summer.indexOf('-') != -1 ||
+						this.summer.indexOf('x') != -1 ||
+						this.summer.indexOf('/') != -1) {
+						this.cal(number);
+						break;
+
+					}
+					this.summer += number;
+
+					break;
+				case '.':
+					break;
+				case '=':
+					break;
+
+			}
+
+			this.result.value = this.summer;
+
+		},
+
+		cal: function(number) {
+			this.summer = '0';
+			this.result.value = this.summer;
+		}
+
 	}
-}	
