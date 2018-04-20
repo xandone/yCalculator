5ae30ae686f52321ad1167ec777137f0dd96eeb9
xandone <765478955@qq.com>
2018-04-20 11:18:41 +0800

    cal

--- cal/js/xmain.js
+++ cal/js/xmain.js
@@ -4,11 +4,15 @@
 		temp: 0,
 		numA: 0,
 		numB: 0,
+		hasSign: false,
+		signNumber: "",
 		summer: '',
 
 		result: document.getElementById('sum'),
 
 		inClick: function(number) {
+
+			var last_str = this.summer.charAt(this.summer.length - 1);
 
 			switch (number) {
 				case '1':
@@ -26,20 +30,31 @@
 				case '-':
 				case 'x':
 				case '/':
-					if (this.summer.indexOf('+') != -1 ||
-						this.summer.indexOf('-') != -1 ||
-						this.summer.indexOf('x') != -1 ||
-						this.summer.indexOf('/') != -1) {
-						this.cal(number);
-						break;
 
+					if (last_str === '+' || last_str === '-' || last_str === 'x' || last_str === '/') {
+						alert('输入有误');
+						break;
 					}
+
+					// 已包含符号,并且不为尾号
+					if (this.hasSign) {
+						this.cal(this.summer, this.signNumber);
+						break;
+					}
+
 					this.summer += number;
+					this.signNumber = number;
+					this.hasSign = true;
 
 					break;
 				case '.':
 					break;
 				case '=':
+					if (last_str === '+' || last_str === '-' || last_str === 'x' || last_str === '/') {
+						alert('输入有误');
+						break;
+					}
+					this.cal(this.summer, this.signNumber);
 					break;
 
 			}
@@ -48,13 +63,32 @@
 
 		},
 
-		cal: function(number) {
-			var last_str = this.summer.charAt(this.summer.length - 1);
-			if (last_str === '+' || last_str === '-' || last_str === 'x' || last_str === '/') {
-				return;
+		cal: function(summer, number) {
+			this.numA = parseInt(summer.substring(0, summer.indexOf(number)));
+			this.numB = parseInt(summer.substring(summer.indexOf(number) + 1));
+
+			this.hasSign = false;
+
+			switch (number) {
+				case '+':
+					summer = (this.numA + this.numB).toString();
+					break;
+				case '-':
+					summer = (this.numA - this.numB).toString();
+					break;
+				case 'x':
+					summer = (this.numA * this.numB).toString();
+					break;
+				case '/':
+					summer = (this.numA / this.numB).toString();
+					break;
 			}
-			this.summer = '0';
+
+			this.summer = summer;
 			this.result.value = this.summer;
+			// 计算一次，重置一次
+			this.hasSign = false;
+			this.signNumber = '';
 		}
 
 	}
