a0185b4338fa8ff9200b2f1a8febdd967e7a8d04
xandone <765478955@qq.com>
2018-08-10 22:51:09 +0800

    ball、canvas

--- drum/js/play.js
+++ drum/js/play.js
@@ -1,5 +1,6 @@
 window.addEventListener('keydown', function(e) {
     console.log(e.key + '的keyCode为：' + e.keyCode);
+    // 常量，必须初始化，不可修改
     const audio = document.querySelector(`.audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     const keys = document.querySelectorAll('.key');
