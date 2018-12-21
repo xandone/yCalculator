baa709ff3edd051242ec3a8ec69e0eb18a21d681
xandone <765478955@qq.com>
2018-12-21 18:38:19 +0800

    layout

--- ttdemo/js/main.js
+++ ttdemo/js/main.js
@@ -1,6 +1,7 @@
 var timer;
 var weather_title = document.getElementsByClassName('weather_title')[0];
 var weather_details = document.getElementById('weather_details');
+var channel_item = document.getElementsByClassName('channel_item');
 
 weather_title.onmouseover = function() {
     weather_details.style.display = 'block';
@@ -17,4 +18,23 @@ weather_details.onmouseover = function() {
 }
 weather_details.onmouseout = function() {
     weather_details.style.display = 'none';
+}
+
+for (var i = 0; i < channel_item.length; i++) {
+    channel_item[i].j = i;
+    channel_item[i].onclick = function() {
+        setChannelSelect(this.j);
+    }
+}
+
+setChannelSelect(0);
+
+function setChannelSelect(index) {
+    for (var i = 0; i < channel_item.length; i++) {
+        if (index == i) {
+            channel_item[i].classname = 'left_title_item_select';
+        } else {
+            channel_item[i].classname = 'channel_item';
+        }
+    }
 }
