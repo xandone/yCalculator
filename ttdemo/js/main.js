104ebfcaf88a50cc5f3de72d03c1f8d32983aa3f
xandone <765478955@qq.com>
2018-12-26 15:07:07 +0800

    layout

--- ttdemo/js/main.js
+++ ttdemo/js/main.js
@@ -1,7 +1,8 @@
 var timer;
 var weather_title = document.getElementsByClassName('weather_title')[0];
 var weather_details = document.getElementById('weather_details');
-var channel_item = document.getElementsByClassName('channel_item');
+var left_title_list = document.getElementById('left_title_list');
+var channel_item = left_title_list.getElementsByTagName('span');
 var news_no_span = document.getElementById('news_no_span');
 var news_no_icon = document.getElementById('news_no_icon');
 
@@ -32,13 +33,11 @@ for (var i = 0; i < channel_item.length; i++) {
 setChannelSelect(0);
 
 function setChannelSelect(index) {
+    console.log(channel_item.length);
     for (var i = 0; i < channel_item.length; i++) {
-        if (index == i) {
-            channel_item[i].className = 'left_title_item_select';
-        } else {
-            channel_item[i].className = 'channel_item';
-        }
+        channel_item[i].className = 'channel_item';
     }
+    channel_item[index].className = 'left_title_item_select';
 }
 
 news_no_icon.onmouseover = function() {
