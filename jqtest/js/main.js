a29c52b08f60f9abe30983bef1ee19290bd67b4b
xandone <765478955@qq.com>
2018-12-29 14:48:55 +0800

    layout

--- jqtest/js/main.js
+++ jqtest/js/main.js
@@ -4,7 +4,7 @@ $(function() {
     getjokeList();
 
     function getjokeList(page, count) {
-        $.ajax(function() {
+        $.ajax({
             url: '/ycat/joke/list',
             type: 'GET',
             dataType: 'json',
@@ -19,10 +19,30 @@ $(function() {
     }
 
     function createJokeList(result) {
-        console.log(result);
-        var jokeItemParent = jokeList.html('ul');
-        var jokeItem = jokeItemParent.html('li');
-        jokeItem.append(result.rows[0].content);
-    }
+        var jokeItemParent = $('<ul></ul>');
+        jokeList.html(jokeItemParent);
 
+        for (var i = 0; i < result.length; i++) {
+            var jokeItem = $('<li></li>');
+            var temp = '<div class="joke-item">' +
+                '<a class="joke-title" href="javascript:;">' + result[i].title + '</a>' +
+                '<div class="joke-author">' +
+                '<img class="joke-author-icon"src="' + result[i].joke_user_icon + '" alt="">' +
+                '<span>' + result[i].joke_user_nick + '</span>' +
+                '</div>' +
+                '<div class="joke-content">' +
+                '<span>' + result[i].content + '</span>' +
+                '</div>' +
+                '<span class="helpful"><img src="../imgs/zan.png" alt="">' + result[i].article_like_count + '</span>' +
+                '<span class="helpful"><img src="../imgs/comment_icon.png" alt="">' + result[i].article_comment_count + '</span>' +
+                '<span class="joke-date">' + result[i].post_time + '</span>' +
+                ' </div>';
+
+            jokeItem.html(temp);
+            jokeItemParent.append(jokeItem);
+
+        }
+
+
+    }
 })
